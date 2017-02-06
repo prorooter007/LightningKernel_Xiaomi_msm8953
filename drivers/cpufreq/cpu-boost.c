40ea80c96e5c38dda325a2baff230d9575fd9129
Francisco Franco <franciscofranco.1990@gmail.com>
2017-02-06 18:03:03 +0000

    cpufreq: cpu-boost: don't boost if input_boost_ms is <= 0
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>
    Signed-off-by: engstk <eng.stk@sapo.pt>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -205,6 +205,10 @@ static void do_input_boost(struct work_struct *work)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	cancel_delayed_work_sync(&dynamic_stune_boost_rem);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
+
+	if (!input_boost_ms)
+		return;
+
 	cancel_delayed_work_sync(&input_boost_rem);
 
 	if (stune_boost_active) {

506e9fe46b2d5a1763369b8b33896f00a95ad0e7
Linux Build Service Account <lnxbuild@localhost>
2018-03-23 01:19:52 -0700

    Merge "cpufreq: schedutil: Add cpu_frequency traces for each cpu in the policy"
    from caf msm-4.14
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -148,6 +148,7 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 				unsigned int next_freq)
 {
 	struct cpufreq_policy *policy = sg_policy->policy;
+	unsigned int cpu;
 
 	if (sg_policy->next_freq == next_freq)
 		return;
@@ -164,7 +165,9 @@ static void sugov_update_commit(struct sugov_policy *sg_policy, u64 time,
 			return;
 
 		policy->cur = next_freq;
-		trace_cpu_frequency(next_freq, smp_processor_id());
+		for_each_cpu(cpu, policy->cpus) {
+			trace_cpu_frequency(next_freq, cpu);
+		}
 	} else {
 		sg_policy->work_in_progress = true;
 		sched_irq_work_queue(&sg_policy->irq_work);

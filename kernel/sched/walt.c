3e9f0554ae698c40d6a0e5f353ef58182032b001
Puja Gupta <pujag@codeaurora.org>
2018-04-17 11:44:32 -0700

    sched/walt: Fix SCHED_CPUFREQ_CONTINUE for hotplug cases
    cluster->cpus might include hotplugged cores and as a result we would
    end up sending incorrect flag to cpufreq. Fix the bug by checking
    cpu_online_mask.
    
    Change-Id: I39fa9296aac72604c91470a8ffe0a5b9efbaac1f
    Signed-off-by: Puja Gupta <pujag@codeaurora.org>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -3292,9 +3292,14 @@ void walt_irq_work(struct irq_work *irq_work)
 		walt_update_coloc_boost_load();
 
 	for_each_sched_cluster(cluster) {
-		unsigned int num_cpus = cpumask_weight(&cluster->cpus), i = 1;
+		cpumask_t cluster_online_cpus;
+		unsigned int num_cpus, i = 1;
 
-		for_each_cpu(cpu, &cluster->cpus) {
+		cpumask_and(&cluster_online_cpus, &cluster->cpus,
+						cpu_online_mask);
+		num_cpus = cpumask_weight(&cluster_online_cpus);
+
+		for_each_cpu(cpu, &cluster_online_cpus) {
 			int flag = SCHED_CPUFREQ_WALT;
 
 			rq = cpu_rq(cpu);

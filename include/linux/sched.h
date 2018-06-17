42464f2426af29c9cfe23a6e22d38820fda2ff8f
joshuous <joshuous@gmail.com>
2018-06-17 18:23:39 +0200

    sched/tune: Introduce stune_boost() function
    Add a simple function to activate Dynamic Schedtune Boost and use the
    dynamic_boost value of the SchedTune CGroup.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -4014,6 +4014,7 @@ unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 int do_stune_boost(char *st_name, int boost);
 int reset_stune_boost(char *st_name);
+int stune_boost(char *st_name);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);

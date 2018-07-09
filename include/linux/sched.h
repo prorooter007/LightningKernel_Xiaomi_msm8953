762c6d2fa99c9f9ed66acb2b6f637dddc05cfe25
joshuous <joshuous@gmail.com>
2018-07-09 17:24:37 +0200

    sched/tune: Switch Dynamic Schedtune Boost to a slot-based tracking system
    Switch from a counter-based system to a slot-based system for managing
    multiple dynamic Schedtune boost requests.
    
    The primary limitations of the counter-based system was that it could
    only keep track of two boost values at a time: the current dynamic boost
    value and the default boost value. When more than one boost request is
    issued, the system would only remember the highest value of them all.
    Even if the task that requested the highest value had unboosted, this
    value is still maintained as long as there are other active boosts that
    are still running. A more ideal outcome would be for the system to
    unboost to the maximum boost value of the remaining active boosts.
    
    The slot-based system provides a solution to the problem by keeping
    track of the boost values of all ongoing active boosts. It ensures that
    the current boost value will be equal to the maximum boost value of
    all ongoing active boosts. This is achieved with two linked lists
    (active_boost_slots and available_boost_slots), which assign and keep
    track of boost slot numbers for each successful boost request. The boost
    value of each request is stored in an array (slot_boost[]), at an index
    value equal to the assigned boost slot number.
    
    For now we limit the number of active boost slots to 5 per Schedtune
    group.
    
    Signed-off-by: joshuous <joshuous@gmail.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -4012,9 +4012,9 @@ unsigned long arch_scale_cpu_capacity(struct sched_domain *sd, int cpu)
 #endif
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
-int do_stune_boost(char *st_name, int boost);
-int do_stune_sched_boost(char *st_name);
-int reset_stune_boost(char *st_name);
+int do_stune_boost(char *st_name, int boost, int *slot);
+int do_stune_sched_boost(char *st_name, int *slot);
+int reset_stune_boost(char *st_name, int slot);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
 extern DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);

4e33712d183396c74117bb6ee72bcf2dd9746d5f
kdrag0n <dragon@khronodragon.com>
2018-10-07 17:16:40 -0700

    printk: silence TEE logs from userspace
    - Spams logs, pushing out useful messages
    - Reduces security because it exposes internal information
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -772,6 +772,11 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 		}
 	}
 
+	if (unlikely(strncmp("healthd", line, 7) == 0 || strncmp("Trustonic TEE", line, 13) == 0))
+	{
+		return len;
+	}
+
 	printk_emit(facility, level, NULL, 0, "%s", line);
 	kfree(buf);
 	return ret;

827d62356a500fa2c627f4ed6a84167c71c1b6fc
Josh Choo <dev@joshuous.com>
2018-12-29 14:25:50 +0700

    Revert "cpufreq: cpu-boost: Force most/all tasks to big cluster on input event"
    sched_boost was deprecated during the removal of HMP scheduling code.
    
    This reverts commit 1c042401d04f47d7c563e6e2d90db0a188815a5c.
    
    [khusika: This also revert commit ce53a57fdc9a9b63576b8b556aefc69250dd2fdd]
    Signed-off-by: khusika <khusikadhamar@gmail.com>
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -49,11 +49,6 @@ module_param(dynamic_stune_boost_ms, uint, 0644);
 static struct delayed_work dynamic_stune_boost_rem;
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 
-static unsigned int sched_boost_on_input;
-module_param(sched_boost_on_input, uint, 0644);
-
-static bool sched_boost_active;
-
 static struct delayed_work input_boost_rem;
 static u64 last_input_time;
 #define MIN_INPUT_INTERVAL (150 * USEC_PER_MSEC)
@@ -176,7 +171,7 @@ static void update_policy_online(void)
 
 static void do_input_boost_rem(struct work_struct *work)
 {
-	unsigned int i, ret;
+	unsigned int i;
 	struct cpu_sync *i_sync_info;
 
 	/* Reset the input_boost_min for all CPUs in the system */
@@ -188,13 +183,6 @@ static void do_input_boost_rem(struct work_struct *work)
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
-
-	if (sched_boost_active) {
-		ret = sched_set_boost(0);
-		if (ret)
-			pr_err("cpu-boost: HMP boost disable failed\n");
-		sched_boost_active = false;
-	}
 }
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
@@ -217,10 +205,6 @@ static void do_input_boost(struct work_struct *work)
 	cancel_delayed_work_sync(&dynamic_stune_boost_rem);
 #endif /* CONFIG_DYNAMIC_STUNE_BOOST */
 	cancel_delayed_work_sync(&input_boost_rem);
-	if (sched_boost_active) {
-		sched_set_boost(0);
-		sched_boost_active = false;
-	}
 
 	if (stune_boost_active) {
 		reset_stune_boost("top-app", boost_slot);
@@ -236,15 +220,6 @@ static void do_input_boost(struct work_struct *work)
 
 	/* Update policies for all online CPUs */
 	update_policy_online();
-
-	/* Enable scheduler boost to migrate tasks to big cluster */
-	if (sched_boost_on_input > 0) {
-		ret = sched_set_boost(sched_boost_on_input);
-		if (ret)
-			pr_err("cpu-boost: HMP boost enable failed\n");
-		else
-			sched_boost_active = true;
-	}
 
 #ifdef CONFIG_DYNAMIC_STUNE_BOOST
 	/* Set dynamic stune boost value */

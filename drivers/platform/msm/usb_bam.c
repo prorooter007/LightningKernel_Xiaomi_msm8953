bddcb9182e4904729127a30e2497cfd4e7118a88
kdrag0n <dragon@khronodragon.com>
2019-01-07 19:14:22 -0800

    usb_bam: disable event log by default
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/platform/msm/usb_bam.c
+++ drivers/platform/msm/usb_bam.c
@@ -44,8 +44,8 @@
 #define ARRAY_INDEX_FROM_ADDR(base, addr) ((addr) - (base))
 
 #define ENABLE_EVENT_LOG 1
-static unsigned int enable_event_log = ENABLE_EVENT_LOG;
-module_param(enable_event_log, uint, 0644);
+static unsigned int enable_event_log;
+module_param(enable_event_log, uint, S_IRUGO | S_IWUSR);
 MODULE_PARM_DESC(enable_event_log, "enable event logging in debug buffer");
 
 #define LOGLEVEL_NONE 8

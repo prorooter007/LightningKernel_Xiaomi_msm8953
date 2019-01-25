0587ec6baffd9ff837a3a75f274cf79154758a62
Hemant Kumar <hemantk@codeaurora.org>
2019-01-25 15:50:07 -0800

    phy-msm-ssusb-qmp: Toggle between xo and pipe clk in/out of LPM
    As per HPG, driver needs to toggle between bi_tcxo and phy pipe
    clock as part of its CX collapse sequence. This is done by
    setting pipe_clk/ref_clk_src as parent of pipe_clk_src.
    
    Change-Id: I353cc55724c8a2bf6b38d4a7f9a1e6f573bf3cab
    Signed-off-by: Hemant Kumar <hemantk@codeaurora.org>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/usb/phy/phy-msm-ssusb-qmp.c
+++ drivers/usb/phy/phy-msm-ssusb-qmp.c
@@ -121,6 +121,8 @@ struct msm_ssphy_qmp {
 	struct clk		*com_aux_clk;
 	struct clk		*cfg_ahb_clk;
 	struct clk		*pipe_clk;
+	struct clk		*pipe_clk_mux;
+	struct clk		*pipe_clk_ext_src;
 	struct reset_control	*phy_reset;
 	struct reset_control	*phy_phy_reset;
 	struct reset_control	*global_phy_reset;
@@ -823,6 +825,14 @@ static int msm_ssphy_qmp_get_clks(struct msm_ssphy_qmp *phy, struct device *dev)
 		goto err;
 	}
 
+	phy->pipe_clk_mux = devm_clk_get(dev, "pipe_clk_mux");
+	if (IS_ERR(phy->pipe_clk_mux))
+		phy->pipe_clk_mux = NULL;
+
+	phy->pipe_clk_ext_src = devm_clk_get(dev, "pipe_clk_ext_src");
+	if (IS_ERR(phy->pipe_clk_ext_src))
+		phy->pipe_clk_ext_src = NULL;
+
 	phy->ref_clk_src = devm_clk_get(dev, "ref_clk_src");
 	if (IS_ERR(phy->ref_clk_src))
 		phy->ref_clk_src = NULL;
@@ -866,12 +876,17 @@ static void msm_ssphy_qmp_enable_clks(struct msm_ssphy_qmp *phy, bool on)
 		if (phy->cfg_ahb_clk)
 			clk_prepare_enable(phy->cfg_ahb_clk);
 
+		//select PHY pipe clock
+		clk_set_parent(phy->pipe_clk_mux, phy->pipe_clk_ext_src);
 		clk_prepare_enable(phy->pipe_clk);
 		phy->clk_enabled = true;
 	}
 
 	if (phy->clk_enabled && !on) {
 		clk_disable_unprepare(phy->pipe_clk);
+
+		//select XO instead of PHY pipe clock
+		clk_set_parent(phy->pipe_clk_mux, phy->ref_clk_src);
 
 		if (phy->cfg_ahb_clk)
 			clk_disable_unprepare(phy->cfg_ahb_clk);

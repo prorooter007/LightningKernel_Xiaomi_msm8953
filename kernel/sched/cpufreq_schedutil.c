ee9499330c7d5a3a2e9e4cbc4663faa1fb181097
Adam W. Willis <return.of.octobot@gmail.com>
2019-02-12 21:48:45 -0500

    cpufreq: schedutil: Hardcode iowait_boost_enable
    Since we are using a more efficient method of iowait boosting, let's enable this feature for improved I/O.
    
    Signed-off-by: Adam W. Willis <return.of.octobot@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -933,6 +933,8 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->down_rate_limit_us *= lat;
 	}
 
+	tunables->iowait_boost_enable = true;
+
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
 	stale_ns = sched_ravg_window + (sched_ravg_window >> 3);

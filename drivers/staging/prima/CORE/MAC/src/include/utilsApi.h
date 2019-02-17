e481b94bb1ca8016473d5cf43dbcd1869f4a502b
laststandrighthere <39502166+laststandrighthere@users.noreply.github.com>
2019-02-17 16:42:44 -0500

    include/utilsApi: Fix misleading identation
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: starlight5234 <ifist2834@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/staging/prima/CORE/MAC/src/include/utilsApi.h
+++ drivers/staging/prima/CORE/MAC/src/include/utilsApi.h
@@ -705,7 +705,7 @@ halRoundS32(tANI_S32 p)
     else
         k = p;
 
-        return(k);
+    return(k);
 }
 
 // New functions for endianess conversion
@@ -735,5 +735,3 @@ void CreateInitScanRawFrame(tpAniSirGlobal pMac, tSirMacMgmtHdr *macMgmtHdr, tBs
 void CreateFinishScanRawFrame(tpAniSirGlobal pMac, tSirMacMgmtHdr *macMgmtHdr, tBssSystemRole role);
 
 #endif /* __UTILSAPI_H */
-
-

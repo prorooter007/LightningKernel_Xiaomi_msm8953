8d0ca1c51dafaf8b7ff607609c236f0151a1c56f
Danny Lin <danny@kdrag0n.dev>
2019-04-03 21:04:22 -0700

    fork: boost CPU/Devfreq to max for 100s when userspace launches an app
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -82,6 +82,9 @@
 #include <linux/cpufreq_times.h>
 #include <linux/cpu_boost.h>
 #include <linux/scs.h>
+#include <linux/binfmts.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2120,8 +2123,13 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	if (is_zygote_pid(current->pid))
-		do_input_boost_max();
+#ifdef CONFIG_CPU_INPUT_BOOST
+	/* Boost CPU to the max for 50 ms when userspace launches an app */
+	if (is_zygote_pid(current->pid) && cpu_input_boost_within_input(75)) {
+		cpu_input_boost_kick_max(100);
+		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 100);
+	}
+#endif
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When

9f937c06e9df21fe482b62bc5f349fb4c7096ca2
Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
2019-05-27 11:57:08 +0100

    drm/atomic: Boost only CPU when committing a new frame
    Signed-off-by: Twisted Prime <36546624+TwistedPrime@users.noreply.github.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -32,7 +32,6 @@
 #include <drm/drm_plane_helper.h>
 #include <linux/sync_file.h>
 #include <linux/cpu_input_boost.h>
-#include <linux/devfreq_boost.h>
 
 #include "drm_crtc_internal.h"
 
@@ -1906,10 +1905,8 @@ int drm_mode_atomic_ioctl(struct drm_device *dev,
 			(arg->flags & DRM_MODE_PAGE_FLIP_EVENT))
 		return -EINVAL;
 
-	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) {
+	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY)) 
 		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-	}
 
 	drm_modeset_acquire_init(&ctx, 0);
 

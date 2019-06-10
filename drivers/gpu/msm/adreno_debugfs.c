f29747745b0d14fbb3e7baf79b57052dc667ec41
Yaroslav Furman <yaro330@gmail.com>
2019-06-10 21:58:27 +0300

    drivers: msm: Don't copy fence names by default
    Same concept as kerneltoast/android_kernel_google_wahoo@fe23bc0
    Extended version that covers more cases.
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    [Lau: removed drm changes and added mdss]
    Signed-off-by: Lau <laststandrighthere@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/gpu/msm/adreno_debugfs.c
+++ drivers/gpu/msm/adreno_debugfs.c
@@ -137,12 +137,14 @@ static void sync_event_print(struct seq_file *s,
 		break;
 	}
 	case KGSL_CMD_SYNCPOINT_TYPE_FENCE: {
+#ifdef CONFIG_FENCE_DEBUG
 		int i;
 
 		for (i = 0; i < sync_event->info.num_fences; i++)
 			seq_printf(s, "sync: %s",
 				sync_event->info.fences[i].name);
 		break;
+#endif
 	}
 	default:
 		seq_printf(s, "sync: type: %d", sync_event->type);

1d0c29adad6203378f824d34e30b555c12d7e3ab
prorooter007 <shreyashwasnik112@gmail.com>
2019-06-22 09:36:06 +0530

    Revert "K-lapse v5.0 :"
    This reverts commit 9b8910906b31989cb870b8d82411d66dfd1e9664.

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -57,10 +57,6 @@
 #include "mdp3_ctrl.h"
 #include "mdss_dsi.h"
 
-#ifdef CONFIG_KLAPSE
-#include <linux/klapse.h>
-#endif
-
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -327,9 +323,6 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 							!mfd->bl_level)) {
 		mutex_lock(&mfd->bl_lock);
 		mdss_fb_set_backlight(mfd, bl_lvl);
-#ifdef CONFIG_KLAPSE
-                set_rgb_slider(bl_lvl);
-#endif
 		mutex_unlock(&mfd->bl_lock);
 	}
 }

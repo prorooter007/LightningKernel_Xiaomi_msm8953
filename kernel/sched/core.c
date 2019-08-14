690f6d41e6b467b8d35f103b77fa82df42a6092f
Danny Lin <danny@kdrag0n.dev>
2019-08-14 05:47:28 +0000

    sched/fair: Make active balance CPU reservations independent of WALT
    There's no reason this should be hidden behind CONFIG_WALT. Nothing else
    is using walt_flags at the moment, so rename it to extra_flags and
    generalize everything so that PELT users can retain the changes.
    
    This is also necessary to make the SDX55 scheduler compile with PELT.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yousef Algadri <yusufgadrie@gmail.com>
    Change-Id: I9964e0e44dfa9fd35ea530ee97bae4e539a75d8c
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8229,6 +8229,7 @@ void __init sched_init(void)
 		rq->max_idle_balance_cost = sysctl_sched_migration_cost;
 		rq->push_task = NULL;
 		set_cpu_isolated(i, false);
+		rq->extra_flags = 0;
 		walt_sched_init_rq(rq);
 
 		INIT_LIST_HEAD(&rq->cfs_tasks);
--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -847,7 +847,6 @@ struct rq {
 	int cstate, wakeup_latency, wakeup_energy;
 	u64 window_start;
 	s64 cum_window_start;
-	unsigned long walt_flags;
 
 	u64 cur_irqload;
 	u64 avg_irqload;
@@ -874,6 +873,8 @@ struct rq {
 	u64 last_cc_update;
 	u64 cycles;
 #endif
+
+	unsigned long extra_flags;
 
 #ifdef CONFIG_IRQ_TIME_ACCOUNTING
 	u64 prev_irq_time;
@@ -2751,7 +2752,6 @@ static inline int same_freq_domain(int src_cpu, int dst_cpu)
 }
 
 #define	BOOST_KICK	0
-#define	CPU_RESERVED	1
 
 extern int sched_boost(void);
 extern int preferred_cluster(struct sched_cluster *cluster,
@@ -2788,27 +2788,6 @@ extern int alloc_related_thread_groups(void);
 extern unsigned long all_cluster_ids[];
 
 extern void check_for_migration(struct rq *rq, struct task_struct *p);
-
-static inline int is_reserved(int cpu)
-{
-	struct rq *rq = cpu_rq(cpu);
-
-	return test_bit(CPU_RESERVED, &rq->walt_flags);
-}
-
-static inline int mark_reserved(int cpu)
-{
-	struct rq *rq = cpu_rq(cpu);
-
-	return test_and_set_bit(CPU_RESERVED, &rq->walt_flags);
-}
-
-static inline void clear_reserved(int cpu)
-{
-	struct rq *rq = cpu_rq(cpu);
-
-	clear_bit(CPU_RESERVED, &rq->walt_flags);
-}
 
 static inline bool
 task_in_cum_window_demand(struct rq *rq, struct task_struct *p)
@@ -2972,7 +2951,6 @@ static inline int same_freq_domain(int src_cpu, int dst_cpu)
 	return 1;
 }
 
-static inline void clear_reserved(int cpu) { }
 static inline int alloc_related_thread_groups(void) { return 0; }
 
 #define trace_sched_cpu_load(...)
@@ -2997,11 +2975,6 @@ static inline int cpu_max_power_cost(int cpu)
 #endif
 
 static inline void clear_walt_request(int cpu) { }
-
-static inline int is_reserved(int cpu)
-{
-	return 0;
-}
 
 static inline int got_boost_kick(void)
 {
@@ -3034,6 +3007,29 @@ static inline void walt_update_min_max_capacity(void) { }
 
 static inline void walt_map_freq_to_load(void) { }
 #endif	/* CONFIG_SCHED_WALT */
+
+#define	CPU_RESERVED	1
+
+static inline int is_reserved(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	return test_bit(CPU_RESERVED, &rq->extra_flags);
+}
+
+static inline int mark_reserved(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	return test_and_set_bit(CPU_RESERVED, &rq->extra_flags);
+}
+
+static inline void clear_reserved(int cpu)
+{
+	struct rq *rq = cpu_rq(cpu);
+
+	clear_bit(CPU_RESERVED, &rq->extra_flags);
+}
 
 static inline bool energy_aware(void)
 {

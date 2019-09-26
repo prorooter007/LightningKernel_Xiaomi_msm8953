92ca612aaf2a98c05c5a59b271c2782fa6173281
mydongistiny <jaysonedson@gmail.com>
2019-09-26 00:45:02 -0700

    sched/walt: enable walt rotate big tasks by default
    Change-Id: I7f518f2d24ce5d46a1217b2377de19cc4cce7846
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/walt.c
+++ kernel/sched/walt.c
@@ -143,7 +143,7 @@ unsigned int __read_mostly walt_disabled = 0;
 
 __read_mostly unsigned int sysctl_sched_cpu_high_irqload = (10 * NSEC_PER_MSEC);
 
-unsigned int sysctl_sched_walt_rotate_big_tasks;
+unsigned int sysctl_sched_walt_rotate_big_tasks = 1;
 unsigned int walt_rotation_enabled;
 
 /*

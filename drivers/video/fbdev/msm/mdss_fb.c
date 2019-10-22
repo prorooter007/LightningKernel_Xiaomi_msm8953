f0453732df824b1b5a22b2e186cdc7973d454ec9
prorooter007 <shreyashwasnik112@gmail.com>
2019-10-22 19:36:30 +0530

    Revert "KLAPSE: adapt for tissot"
    This reverts commit c5a15b57838db2704dba4928fab04a446086924f.

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -58,10 +58,6 @@
 #include "mdp3_ctrl.h"
 #include "mdss_sync.h"
 
-#ifdef CONFIG_KLAPSE
-#include "klapse.h"
-#endif
-
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
 #define MDSS_FB_NUM 3
 #else
@@ -343,10 +339,6 @@ static void mdss_fb_set_bl_brightness(struct led_classdev *led_cdev,
 		mdss_fb_set_backlight(mfd, bl_lvl);
 		mutex_unlock(&mfd->bl_lock);
 	}
-
-#ifdef CONFIG_KLAPSE
-	set_rgb_slider(bl_lvl);
-#endif
 }
 
 static enum led_brightness mdss_fb_get_bl_brightness(

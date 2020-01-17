6af9b0d12d611241e653e37b389658f6196cd976
ktommy91 <kemenesi.tamas@gmail.com>
2020-01-17 19:32:03 +0100

    cpufreq: schedutil: Set the google's up/down_rate_limit_us values by default
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -769,8 +769,8 @@ static int sugov_init(struct cpufreq_policy *policy)
          * intializing up_rate/down_rate to 0 explicitly in kernel
          * since WALT expects so by default.
          */
-        tunables->up_rate_limit_us = 0;
-        tunables->down_rate_limit_us = 0;
+        tunables->up_rate_limit_us = 500;
+        tunables->down_rate_limit_us = 20000;
 
 	tunables->iowait_boost_enable = false;
 	policy->governor_data = sg_policy;

de803bcd5209da7b7f352ff956b55da500208dde
prorooter007 <shreyashwasnik112@gmail.com>
2020-03-28 13:01:52 +0530

    f2fs:fix mismerge
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- fs/crypto/bio.c
+++ fs/crypto/bio.c
@@ -38,11 +38,12 @@ static void __fscrypt_decrypt_bio(struct bio *bio, bool done)
 		} else {
 			int ret = fscrypt_decrypt_pagecache_blocks(page, bv->bv_len,
 							           bv->bv_offset);
-			if (ret)
+			if (ret) {
 				WARN_ON_ONCE(1);
 				SetPageError(page);
-			else if (done)
+			} else if (done) {
 				SetPageUptodate(page);
+	                }
 		}
 		if (done)
 			unlock_page(page);

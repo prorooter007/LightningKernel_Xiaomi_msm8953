f4f94c77d3ea1ebda5972f05cbb3456777afab2a
LineageOS <lineageos-buildbot@docker.host>
2020-05-05 00:43:31 +0000

    drivers: input: silence fpc1020 spam
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/input/fingerprint/fpc_tissot/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tissot/fpc1020_tee.c
@@ -581,7 +581,7 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 {
 	struct fpc1020_data *fpc1020 = handle;
 
-	dev_err(fpc1020->dev, "%s\n", __func__);
+	dev_dbg(fpc1020->dev, "%s\n", __func__);
 
 	if (atomic_read(&fpc1020->wakeup_enabled)) {
 		__pm_wakeup_event(&fpc1020->ttw_wl,

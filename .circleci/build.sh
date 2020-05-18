86fb230a9747bde9cba9dd4474d258e6dc90c54e
prorooter007 <shreyashwasnik112@gmail.com>
2020-05-18 22:47:54 +0530

    CircleCi: change lightning branch
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- .circleci/build.sh
+++ .circleci/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Cloning dependencies"
-git clone --depth=1 -b stablereb https://github.com/prorooter007/LightningKernel_Xiaomi_msm8953 kernel
+git clone --depth=1 -b stablepelt https://github.com/prorooter007/LightningKernel_Xiaomi_msm8953 kernel
 cd kernel
 git clone --depth=1 -b master https://github.com/kdrag0n/proton-clang clang
 git clone https://github.com/prorooter007/AnyKernel3 -b tissot --depth=1 AnyKernel

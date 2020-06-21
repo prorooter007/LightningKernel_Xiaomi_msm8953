d84d6d63a4506a68fd0f5607c30d885ba613ce19
Pavankumar Kondeti <pkondeti@codeaurora.org>
2020-06-21 14:15:17 +0530

    sched: use the CPU true capacity while sorting the min/mid/max CPUs
    The current code uses the rq->cpu_capacity_orig while sorting
    the min/mid/max CPUs while rebuilding the scheduler domains. This
    rq->cpu_capacity_orig is subjected to changed when the scaling max
    frequency is clipped for that CPU's frequency domain. Since we don't
    recompute the min/mid/max CPUs when the capacity is changed, the
    sorting becomes incorrect later when the frequency limits are lifted.
    
    The task CPU selection algorithm depends heavily on min/mid/max CPUs
    and changing them on the fly results in incorrect task placement.
    Hence use the true capacity of CPUs while sorting the min/mid/max
    CPUs. This means that the sorting gets changed only when CPUs are
    hotplugged out, otherwise these reflect the correct topology
    all the time.
    
    The max_cpu_capacity struct in root domain also maintains the
    max CPU and its capacity. Since we now use true capacity, there
    is no need to compute this on the fly. So move this evaluation from
    the periodic load balancer to the scheduler domain rebuilding.
    
    The rq->cpu_capacity_orig is still subjected to change upon frequency
    or thermal limits. So we still identify the cases where tasks not
    fitting on the max/mid CPUs when the capacity is reduced.
    
    Change-Id: I42735accd079b2ece1eb58d1ebcf322d454e33a2
    Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>
    [Fixup minor merge error]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -7782,10 +7782,33 @@ static int build_sched_domains(const struct cpumask *cpu_map,
 	/* Attach the domains */
 	rcu_read_lock();
 	for_each_cpu(i, cpu_map) {
+		int max_cpu = READ_ONCE(d.rd->max_cap_orig_cpu);
+		int min_cpu = READ_ONCE(d.rd->min_cap_orig_cpu);
+
+		if ((max_cpu < 0) || (arch_scale_cpu_capacity(NULL, i) >
+				arch_scale_cpu_capacity(NULL, max_cpu)))
+			WRITE_ONCE(d.rd->max_cap_orig_cpu, i);
+
+		if ((min_cpu < 0) || (arch_scale_cpu_capacity(NULL, i) <
+				arch_scale_cpu_capacity(NULL, min_cpu)))
+			WRITE_ONCE(d.rd->min_cap_orig_cpu, i);
+
 		sd = *per_cpu_ptr(d.sd, i);
 
 		cpu_attach_domain(sd, d.rd, i);
 	}
+
+	/*
+	 * The max_cpu_capacity reflect the original capacity which does not
+	 * change dynamically. So update the max cap CPU and its capacity
+	 * here.
+	 */
+	if (d.rd->max_cap_orig_cpu != -1) {
+		d.rd->max_cpu_capacity.cpu = d.rd->max_cap_orig_cpu;
+		d.rd->max_cpu_capacity.val = arch_scale_cpu_capacity(NULL,
+						d.rd->max_cap_orig_cpu);
+	}
+
 	rcu_read_unlock();
 
 	ret = 0;

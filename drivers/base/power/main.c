3899029839243fa3f3d6f80d54ffd7d3b3903c5f
Saurabh Charde <saurabhchardereal@gmail.com>
2020-07-26 16:28:10 +0530

    drivers: base: power: Silence PM suspend/resume log spam
    [  421.240160] PM: suspend of devices complete after 14.559 msecs
    [  421.244236] PM: late suspend of devices complete after 4.065 msecs
    [  421.248763] PM: noirq suspend of devices complete after 4.519 msecs
    [  421.441164] PM: noirq resume of devices complete after 2.332 msecs
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -438,7 +438,7 @@ static void dpm_show_time(ktime_t starttime, pm_message_t state, int error,
 	usecs = usecs64;
 	if (usecs == 0)
 		usecs = 1;
-	pr_info("PM: %s%s%s of devices complete after %ld.%03ld msecs\n",
+	pr_debug("PM: %s%s%s of devices complete after %ld.%03ld msecs\n",
 		  info ?: "", info ? " " : "", pm_verb(state.event),
 		  error ? "aborted" : "complete",
 		  usecs / USEC_PER_MSEC, usecs % USEC_PER_MSEC);

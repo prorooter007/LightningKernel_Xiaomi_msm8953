4bca088cbcca99bb5a5bc53092f356aa1dab4e53
Aarqw12 <lcockx@protonmail.com>
2020-07-29 11:38:38 +0000

    cpufreq : ondemand : tune threshold
    * tune the governor to be more power efficient by simply changing the up_threshold to 98
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/cpufreq/cpufreq_ondemand.c
+++ drivers/cpufreq/cpufreq_ondemand.c
@@ -20,7 +20,7 @@
 #include "cpufreq_ondemand.h"
 
 /* On-demand governor macros */
-#define DEF_FREQUENCY_UP_THRESHOLD		(80)
+#define DEF_FREQUENCY_UP_THRESHOLD		(98)
 #define DEF_SAMPLING_DOWN_FACTOR		(1)
 #define MAX_SAMPLING_DOWN_FACTOR		(100000)
 #define MICRO_FREQUENCY_UP_THRESHOLD		(95)

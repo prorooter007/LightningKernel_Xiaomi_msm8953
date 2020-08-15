8c41c0c3e9e0b321008d9fdbae380af45f0f7a1c
prorooter007 <shreyashwasnik112@gmail.com>
2020-08-15 14:09:57 +0530

    cpufreq: re-add SCHED_CPUFREQ_RT_DL
    "Dependency for blu_schedutil"
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- include/linux/sched/cpufreq.h
+++ include/linux/sched/cpufreq.h
@@ -12,6 +12,8 @@
 #define SCHED_CPUFREQ_EARLY_DET	(1U << 6)
 #define SCHED_CPUFREQ_CONTINUE (1U << 8)
 
+#define SCHED_CPUFREQ_RT_DL	(SCHED_CPUFREQ_RT | SCHED_CPUFREQ_DL)
+
 #ifdef CONFIG_CPU_FREQ
 struct update_util_data {
        void (*func)(struct update_util_data *data, u64 time, unsigned int flags);

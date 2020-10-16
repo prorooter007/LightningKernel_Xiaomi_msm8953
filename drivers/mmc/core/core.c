25d63f7a78392d34d295ec08511f7403bea0c764
Jacob Deepu <d2jdeepu@gmail.com>
2020-10-16 12:08:01 +0530

    mmc: core: Don't retune after un-gating the clocks for hynix emmc
    On Hynix eMMC HAG2e tuning execution failing for some
    unknown reason. Triggering retuning again causing random
    reboot on these cards.
    
    These change disables re-tuning whenever clock is ungated
    for all Hynix emmc cards as these will not affect any
    other hynix emmc card.
    
    Change-Id: I0a235c7a47338bdf2203a9a0cf58162cd1167045
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- drivers/mmc/core/core.c
+++ drivers/mmc/core/core.c
@@ -2568,6 +2568,8 @@ void mmc_gate_clock(struct mmc_host *host)
  */
 void mmc_ungate_clock(struct mmc_host *host)
 {
+	struct mmc_card *card = host->card;
+
 	/*
 	 * We should previously have gated the clock, so the clock shall
 	 * be 0 here! The clock may however be 0 during initialization,
@@ -2585,7 +2587,8 @@ void mmc_ungate_clock(struct mmc_host *host)
 		 * To workaround this issue, we are triggering retuning of the
 		 * tuning circuit after ungating the controller clocks.
 		 */
-		mmc_retune_needed(host);
+		if (card->cid.manfid != CID_MANFID_HYNIX)
+			mmc_retune_needed(host);
 	}
 }
 

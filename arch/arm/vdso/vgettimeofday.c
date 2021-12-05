45339c5fb8b2c62ef853e749925509ecffce4853
prorooter007 <shreyashwasnik112@gmail.com>
2021-12-05 22:08:35 +0530

    Revert "FROMLIST: [PATCH v5 08/12] arm: vdso: Add ARCH_CLOCK_FIXED_MASK"
    This reverts commit 50ea379c00b4b579698de7c434ef207cbc2c8e53.
    
    Signed-off-by: prorooter007 <shreyashwasnik112@gmail.com>

--- arch/arm/vdso/vgettimeofday.c
+++ arch/arm/vdso/vgettimeofday.c
@@ -131,11 +131,7 @@ static notrace int do_realtime(const struct vdso_data *vd, struct timespec *ts)
 {
 	u32 seq, mult, shift;
 	u64 nsec, cycle_last;
-#ifdef ARCH_CLOCK_FIXED_MASK
-	static const u64 mask = ARCH_CLOCK_FIXED_MASK;
-#else
 	u64 mask;
-#endif
 	vdso_xtime_clock_sec_t sec;
 
 	do {
@@ -148,9 +144,7 @@ static notrace int do_realtime(const struct vdso_data *vd, struct timespec *ts)
 
 		mult = vd->cs_mono_mult;
 		shift = vd->cs_shift;
-#ifndef ARCH_CLOCK_FIXED_MASK
 		mask = vd->cs_mask;
-#endif
 
 		sec = vd->xtime_clock_sec;
 		nsec = vd->xtime_clock_snsec;
@@ -170,11 +164,7 @@ static notrace int do_monotonic(const struct vdso_data *vd, struct timespec *ts)
 {
 	u32 seq, mult, shift;
 	u64 nsec, cycle_last;
-#ifdef ARCH_CLOCK_FIXED_MASK
-	static const u64 mask = ARCH_CLOCK_FIXED_MASK;
-#else
 	u64 mask;
-#endif
 	vdso_wtm_clock_nsec_t wtm_nsec;
 	__kernel_time_t sec;
 
@@ -188,9 +178,7 @@ static notrace int do_monotonic(const struct vdso_data *vd, struct timespec *ts)
 
 		mult = vd->cs_mono_mult;
 		shift = vd->cs_shift;
-#ifndef ARCH_CLOCK_FIXED_MASK
 		mask = vd->cs_mask;
-#endif
 
 		sec = vd->xtime_clock_sec;
 		nsec = vd->xtime_clock_snsec;
@@ -215,11 +203,7 @@ static notrace int do_monotonic_raw(const struct vdso_data *vd,
 {
 	u32 seq, mult, shift;
 	u64 nsec, cycle_last;
-#ifdef ARCH_CLOCK_FIXED_MASK
-	static const u64 mask = ARCH_CLOCK_FIXED_MASK;
-#else
 	u64 mask;
-#endif
 	vdso_raw_time_sec_t sec;
 
 	do {
@@ -232,9 +216,7 @@ static notrace int do_monotonic_raw(const struct vdso_data *vd,
 
 		mult = vd->cs_raw_mult;
 		shift = vd->cs_shift;
-#ifndef ARCH_CLOCK_FIXED_MASK
 		mask = vd->cs_mask;
-#endif
 
 		sec = vd->raw_time_sec;
 		nsec = vd->raw_time_nsec;
